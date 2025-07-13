aabcdffdbf5f023f6fb49c89d9cc43109c50cdd7
kushbharti <try.kushbharti@gmail.com>
2025-07-13 20:25:11 +0530

    JWTAuthentication

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from . import views
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 
 router = routers.DefaultRouter()
 router.register(r'blogs',views.BlogsViewset)
@@ -8,4 +9,7 @@ router.register(r'comments',views.CommentsViewset)
 
 urlpatterns = [
     path('',include(router.urls)),
+    path('gettoken/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('refreshtoken/',TokenRefreshView.as_view(),name='token_refresh'),
+    path('verifytoken/',TokenVerifyView.as_view(),name='verify_token'),
 ]
--- Blogs/views.py
+++ Blogs/views.py
@@ -3,12 +3,16 @@ from .serializers import Blogs_Serializer,Comments_Serializer
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+# from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class BlogsViewset(viewsets.ModelViewSet):
     queryset = Blogs.objects.all()
     serializer_class = Blogs_Serializer
+    # authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     filter_backends = [SearchFilter]
     search_fields = ['blog_title']
     
